2c35f05df95917a4b7b6d8655437170b1a9b8ce0
hernang <hernangalvan17@gmail.com>
2014-05-28 14:16:28 -0500

    Ahora ya esta con el branch y simbolo de dirty a la derecha

--- config/.config/fish/functions/fish_prompt.fish
+++ config/.config/fish/functions/fish_prompt.fish
@@ -20,8 +20,7 @@ function fish_right_prompt
   set_color blue -o
   printf '[%s] ' (hostname)
 
-if not set -q -g __fish_robbyrussell_functions_defined
-  set -g __fish_robbyrussell_functions_defined
+
 function _git_branch_name
     echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
   end
@@ -29,7 +28,7 @@ function _git_branch_name
   function _is_git_dirty
     echo (git status -s --ignore-submodules=dirty ^/dev/null)
   end
-  end
+  
 
   set -l red (set_color -o red)
   set -l yellow (set_color -o yellow)
